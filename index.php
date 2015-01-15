c356fa008e6d4b7a575c43d732c8d6e1ac288e92
rscanham <info@ryanscottcanham.com>
2015-01-15 16:52:39 +0000

    Navigation setup

--- index.php
+++ index.php
@@ -6,8 +6,12 @@
     </head>
 
     <body>
-
-    <h1>Hello</h1>
+	    <nav>
+	    	<h1 class="logo">Ryan Canham - Senior designer London.</h1>
+	    </nav>
+	    <section>
+		    <h1>Hi, Let me introduce myself, I'm Ryan Canham, senior designer in London.</h1>
+		</section>
 
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
