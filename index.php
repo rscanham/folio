48b21fd324630c27b1900af0b3603799eee762ca
rscanham <info@ryanscottcanham.com>
2015-01-15 16:41:50 +0000

    Basic Setup

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/folio.css"  media="screen,projection"/>
+    </head>
+
+    <body>
+
+    <h1>Hello</h1>
+
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+  </html>
